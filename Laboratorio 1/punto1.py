697d630b80a7b9cf4b8343dab676737e6c0b4ade
Daniel Diaz <d.diazm@uniandes.edu.co>
2024-08-16 17:26:46 +0000

    Lab 1: punto 1

---
+++ Laboratorio 1/punto1.py
@@ -0,0 +1,77 @@
+"""
+    Problema 1:
+        Asignación de Tareas en un Equipo de Desarrollo Ágil
+
+    Descripción:
+        Un Scrum Master necesita asignar un conjunto de tareas
+        a un equipo de desarrollo (pod) basado en la prioridad
+        de las tareas y los puntos de historia asociados.
+        El equipo consiste en 4 desarolladores, y la duración
+        del sprint suele ser de 2 semanas. Por regla general
+        cada desarollador no debe tener mas de 24 puntos de
+        historia asociados a ellos mismos. Con base a esta
+        información realiza un modelo de optimización que tome
+        las tareas que se pueden realizar en este sprint
+        teniendo en cuenta que queremos maximiar la cantidad
+        de tareas realizadas con la mayor prioridad.
+
+    Nota:
+        Para simplificar el problema, la restricción que se
+        usará es que el número máximo de puntos no debe ser
+        superior a los puntos máximos por trabajador
+        multiplicado por el número de trabajadores
+
+    Solución:
+        @Author: ddi4z
+"""
+
+from pyomo.environ import *
+from pyomo.opt import SolverFactory
+
+numeroDesarrolladores = 4
+puntosMaximosPorDesarrollador = 24
+
+puntosPorTarea = [5,3,13,1,21,2,2,5,8,13,21]
+
+prioridadPorTarea = [
+    "Maxima",
+    "Media alta",
+    "Alta",
+    "Media baja",
+    "Minima",
+    "Media",
+    "Alta",
+    "Media",
+    "Baja",
+    "Maxima",
+    "Alta"
+]
+
+valorPorPrioridad = {
+    "Maxima": 7,
+    "Alta": 6,
+    "Media alta": 5,
+    "Media": 4,
+    "Media baja": 3,
+    "Baja": 2,
+    "Minima": 1
+}
+
+M = ConcreteModel()
+
+T = RangeSet(0,10)
+
+M.x = Var(T, domain=Binary)
+
+M.obj = Objective(expr = sum(M.x[t] * valorPorPrioridad[prioridadPorTarea[t]] for t in T), sense=maximize)
+
+M.res = Constraint(expr = sum(M.x[t]* puntosPorTarea[t] for t in T) <= puntosMaximosPorDesarrollador * numeroDesarrolladores )
+
+SolverFactory('glpk').solve(M)
+
+M.display()
+
+for t in T:
+    if M.x[t]() == 1:
+        print(f"La tarea {t} fue elegida, con prioridad {prioridadPorTarea[t]} y {puntosPorTarea[t]} puntos.")
+
